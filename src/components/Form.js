8041c7a69fb28cc14e9bcab49cfa1595879ab2f2
padana <ahmadanote26@gmail.com>
2021-01-02 01:09:20 +0100

    Une application pour  connaitre la temperature de votre ville

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ export default function Form(props) {
     return (
         <div className ='d-flex justify-content-center'>
             {props.error ? Error() : null}
+            <div>
             <form onSubmit ={props.onSubmit} className ='mt-5 mb-3 row d-flex justify-content-center'>
                 <div className="col-md-5 col-5" >
                     <input type="text" 
@@ -25,13 +26,14 @@ export default function Form(props) {
                 </div>
                 <button type="submit" className="btn btn-warning for-group  col-md-2 offset-col-3 m-3">Meteo</button>
             </form>  
+            </div>
         </div>
     )
 
     function Error(){
         return(
-            <div className ='text-center alert alert-warning alert-dismissible fade show mt-5' role="alert">
-                Attention veuiller les champs ci-dessous
+            <div className ='text-center alert alert-warning fade show mt-5' role="alert">
+                <h6>Attention veuiller les champs ci-dessous</h6>
             </div>
 
         )
